1ac438cc47c2008e2b52aefc43c5c8551f435a72
seongmin <0411tjdals34@gmail.com>
2024-11-24 01:16:29 +0900

    test: 프로토타입 - 싱글톤 빈과 함께 사용시 문제점

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -22,6 +22,33 @@ public class SingletonWithPrototypeTest1 {
         assertThat(prototypeBena2.getCount()).isEqualTo(1);
     }
 
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        public ClientBean(final PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
